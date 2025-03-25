f29c2ca6794c4324e4c83b6cfb27e1657177eb38
amiraBENMOU <amiraben2010@hotmail.fr>
2025-03-26 00:06:14 +0100

    Add new project entries with updated descriptions and images in Project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,8 +3,8 @@ import mock01 from '../assets/images/mock01.png';
 import mock02 from '../assets/images/mock02.png';
 import mock03 from '../assets/images/mock03.png';
 import mock04 from '../assets/images/mock04.png';
-//import mock05 from '../assets/images/mock05.png';
-//import mock06 from '../assets/images/mock06.png';
+import mock05 from '../assets/images/mock05.png';
+import mock06 from '../assets/images/mock06.png';
 //import mock07 from '../assets/images/mock07.png';
 //import mock08 from '../assets/images/mock08.png';
 //import mock09 from '../assets/images/mock09.png';
@@ -16,6 +16,19 @@ function Project() {
     <div className="projects-container" id="projects">
         <h1>Personal Projects</h1>
         <div className="projects-grid">
+        <div className="project">
+                <a href="https://whimsical-smakager-54526e.netlify.app/" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://whimsical-smakager-54526e.netlify.app/" target="_blank" rel="noreferrer"><h2>AIESEC Landing  Page</h2></a>
+                <p>This is a landing page created with React js .</p>
+            </div>
+            <div className="project">
+                <a href="https://youtu.be/lZj7ClqxixY" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://youtu.be/lZj7ClqxixY" target="_blank" rel="noreferrer"><h2>Khedemni Platform</h2></a>
+                <p>This is a service platform.  My target is mainly the students and fresh graduates. You will find three types of works that are ; part time jobs, internships and PFE. This platform was developed using html ,css ,javascripy react.js , node.js , material UI , react bootstrap and I also used third-party APIs , redux, and hbs to create reports and multer to upload pdf’s .
+                 The database is a Nosql database created with MongoDB .
+
+                </p>
+            </div>
             <div className="project">
                 <a href="https://brilliant-pegasus-484ac5.netlify.app/" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://brilliant-pegasus-484ac5.netlify.app/" target="_blank" rel="noreferrer"><h2>Saksouni 3la ITC</h2></a>
@@ -37,36 +50,8 @@ function Project() {
                 <a href="https://jocular-syrniki-d5cd66.netlify.app/" target="_blank" rel="noreferrer"><h2>Amazon Clone</h2></a>
                 <p>This is a page of the amazon website that I cloned it using Html5,CSS3 when I was learning web devlopment .</p>
             </div>
-            <div className="project">
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><h2>WeManage: Real Estate Asset Management</h2></a>
-                <p>This mobile application allows realtors in Japan to securely manage their property information and view future income predictions. This app is built with Ruby on Rails and JavaScript.</p>
-            </div>
-            <div className="project">
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><h2>COVID-19 Case Management</h2></a>
-                <p>Built official charts for COVID/vaccination tracking for an educational institution using JavaScript and the Google Sheets API v4. The dashboard served the university's leadership in their decision-making processes.</p>
-            </div>
-            <div className="project">
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><h2>Multiple Regression Property Analysis</h2></a>
-                <p>Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.</p>
-            </div>
-            <div className="project">
-                <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><h2>Programs of Study</h2></a>
-                <p>Designed and developed a custom component for a CMS-based platform (e.g., 'Brightspot') using Java, Handlebars, and LESS. University students can find their majors of interest through this module.</p>
-            </div>
-            <div className="project">
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><h2>Transfer Evaluation Matrix</h2></a>
-                <p>Created an interactive CSV table generator with Java, Handlebars, and LESS. This project helps transfer students to quickly identify eligible credits.</p>
-            </div>
-            <div className="project">
-                <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><h2>Submeowrine</h2></a>
-                <p>Developed and released an Android mobile application using Java and Android Studio that runs a 2D shooting game.</p>
-            </div>
+           
+          
         </div>
     </div>
     );
